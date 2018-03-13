6d97ed7b5ea481bf38949484f31bac409f7fd769
stroupbslayen <strouprd@gmail.com>
2018-03-13 15:47:17 -0700

    Update pubg.py

--- cogs/pubg.py
+++ cogs/pubg.py
@@ -6,27 +6,28 @@ from bs4 import BeautifulSoup as soup
 bot=commands.Bot(command_prefix='!')
 
 modes = {
-			'solo':{
-				'name':'tpp','size':1},
-			'duo':{
-				'name':'tpp','size':2},
-			'squad':{
-				'name':'tpp','size':4},
-			'solo-fpp':{
-				'name':'fpp','size':1},
-			'duo-fpp':{
-				'name':'fpp','size':2},
-			'squad-fpp':{
-				'name':'fpp','size':4}
+		'solo':{
+			'name':'tpp','size':1},
+		'duo':{
+			'name':'tpp','size':2},
+		'squad':{
+			'name':'tpp','size':4},
+		'solo-fpp':{
+			'name':'fpp','size':1},
+		'duo-fpp':{
+			'name':'fpp','size':2},
+		'squad-fpp':{
+			'name':'fpp','size':4}
 		}
 
-regions={	'na':'[NA] North America',
-			'as':'[AS] Asia',
-			'sea':'[SEA] South East Asia',
-			'krjp':'[KRJP] Korea/Japan',
-			'oc':'[OC] Oceania',
-			'sa':'[SA] South America',
-			'eu':'[EU] Europe'
+regions={
+		'na':'[NA] North America',
+		'as':'[AS] Asia',
+		'sea':'[SEA] South East Asia',
+		'krjp':'[KRJP] Korea/Japan',
+		'oc':'[OC] Oceania',
+		'sa':'[SA] South America',
+		'eu':'[EU] Europe'
 		}
 
 class default:
@@ -39,12 +40,11 @@ class pubg():
 
 	async def no_player(self):
 		embed=discord.Embed(title='Error', description='No player name was entered.',color=0xff0000)
-		return embed
+		await self.bot.say(embed=embed)
 
 	async def error_message(self,playername,message):
-		embed= discord.Embed(title='Message from PUBG.OP.GG:', description='Error for player {}:\n{}'.format(playername.upper(),message),color=0xff0000)
-		embed.to_dict()
-		return embed
+		embed= discord.Embed(title='Message from PUBG.OP.GG:', description='Error for player {}:\n{}'.format(playername,message),color=0xff0000)
+		await self.bot.say(embed=embed)
 
 	async def PUBG_API(self,url):
 		try:
@@ -63,7 +63,7 @@ class pubg():
 		#get line with player ID
 		pID=page.find('div',{'class':'player-summary__name'})
 		if pID is None:
-			return {'error':'player not found'}
+			return {'error':'Player not found'}
 		playerid=pID['data-user_id']
 		nickname=pID['data-user_nickname']
 		#get line with current season
@@ -72,70 +72,66 @@ class pubg():
 		return {'playerid':playerid,'season':season,'nickname':nickname}
 
 	@commands.command()
-	async def stats(self,*,message:str):
+	async def stats(self,*,message:str=None):
 		''': Get stats for a PUBG player'''
-			mode=None
-			region=None
-			players=[]
-			output=[]
+		mode=None
+		region=None
+		players=[]
 
-			#message if no data is entered
-			if message==None:
-				msg=await no_player()
-				await self.bot.say(embed=msg)
-				return
-			message = message.lower().split()
 
-			#get player region and match type, if none are put in it uses default values
-			for data in message:
-				if data in modes:
-					mode=data
-				if data in regions:
-					region=data
-				if data not in (list(modes)+list(regions)):
-					players.append(data)
+		#message if no data is entered
+		if message==None:
+			msg=await self.no_player()
+			return
 
-			#sets search criteria if none is input, uses default values
-			if mode==None:
-				mode=default.mode
-			if region==None:
-				region=default.region
+		message = message.lower().split()
 
-			#check if playernames are entered
-			if len(players)==0:
-				msg=await self.no_player()
-				await self.bot.say(embed=msg)
-				return
+		#get player region and match type, if none are put in it uses default values
+		for data in message:
+			if data in modes:
+				mode=data
+			if data in regions:
+				region=data
+			if data not in (list(modes)+list(regions)):
+				players.append(data)
 
-			else:
-				for player in players:
-					data = await self.getID(player)
-					if 'error' not in data:
-						class profile:
-							nickname=data['nickname']
-							playerid=data['playerid']
-							season=data['season']
-							server=region
-							match=modes[mode]['name']
-							size=modes[mode]['size']
-						url='https://pubg.op.gg/api/users/{0.playerid}/ranked-stats?season={0.season}&server={0.server}&queue_size={0.size}&mode={0.match}'.format(profile)
-						player_stats = await self.PUBG_API(url)
-						if 'message' in player_stats:
-							error=await self.error_message(player,'Player has no games with these stats')
-							await self.bot.say(embed=error)
-							break
-						else:
-							embed = discord.Embed(title=profile.nickname,url='https://pubg.op.gg/user/{0.nickname}?server={0.server}'.format(profile), description='{}-{}\nGrade: **{}**\nSeason: {}'.format(regions[region],mode.upper(),player_stats['grade'],profile.season) , color=0x00ff00)
-							embed.add_field(name="Rank", value=player_stats['ranks']['rating'], inline=True)
-							embed.add_field(name="Skill Rating", value=player_stats['stats']['rating'], inline=True)
-							embed.add_field(name="Wins",value=player_stats['stats']['win_matches_cnt'], inline=True)
-							embed.add_field(name="Win Rate", value='{}%'.format(round(player_stats['stats']['win_matches_cnt']/player_stats['stats']['matches_cnt']*100,2), inline=True))
-							embed.add_field(name='Kills', value=player_stats['stats']['kills_sum'],inline=True)
-							embed.add_field(name="K/D", value=round(player_stats['stats']['kills_sum']/player_stats['stats']['deaths_sum'],2), inline=True)
-							await self.bot.say(embed=embed)
+		#sets search criteria if none is input, uses default values
+		if mode==None:
+			mode=default.mode
+		if region==None:
+			region=default.region
+
+		#check if playernames are entered
+		if len(players)==0:
+			await self.no_player()
+			return
+
+		else:
+			for player in players:
+				data = await self.getID(player)
+				if 'error' not in data:
+					class profile:
+						nickname=data['nickname']
+						playerid=data['playerid']
+						season=data['season']
+						server=region
+						match=modes[mode]['name']
+						size=modes[mode]['size']
+					url='https://pubg.op.gg/api/users/{0.playerid}/ranked-stats?season={0.season}&server={0.server}&queue_size={0.size}&mode={0.match}'.format(profile)
+					player_stats = await self.PUBG_API(url)
+					if 'message' in player_stats:
+						await self.error_message(profile.nickname,'Player has no stats for {}-{}'.format(regions[region],mode.upper()))
 					else:
-						error=await self.error_message(player,'Playername not found')
-						await self.bot.say(embed=error)
+						embed = discord.Embed(title=profile.nickname,url='https://pubg.op.gg/user/{0.nickname}?server={0.server}'.format(profile), description='{}-{}\nGrade: **{}**\nSeason: {}'.format(regions[region],mode.upper(),player_stats['grade'],profile.season) , color=0x00ff00)
+						embed.add_field(name="Rank", value=player_stats['ranks']['rating'], inline=True)
+						embed.add_field(name="Skill Rating", value=player_stats['stats']['rating'], inline=True)
+						embed.add_field(name="Wins",value=player_stats['stats']['win_matches_cnt'], inline=True)
+						embed.add_field(name="Win Rate", value='{}%'.format(round(player_stats['stats']['win_matches_cnt']/player_stats['stats']['matches_cnt']*100,2), inline=True))
+						embed.add_field(name='Kills', value=player_stats['stats']['kills_sum'],inline=True)
+						embed.add_field(name="K/D", value=round(player_stats['stats']['kills_sum']/player_stats['stats']['deaths_sum'],2), inline=True)
+						await self.bot.say(embed=embed)
+				else:
+					error=await self.error_message(player,data['error'])
 
 
 def setup(bot):
